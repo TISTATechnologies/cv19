0424a6c153d40dc6f7fba83e86fa013bf703321c
Evgen Rusakov <erusakov@cv19.tistatech.com>
2020-08-31 13:17:54 -0400

    Add email notification to all GitHub actions

--- .github/scripts/send-build-notification.sh
+++ .github/scripts/send-build-notification.sh
@@ -2,6 +2,11 @@
 ###############################################################################
 # Helper script: send email notification from the GitHub Actions job
 ###############################################################################
+read_body() {
+    # Read body from the file or stdin
+    if [ -n "${1}" ]; then grep -v '^[[:space:]]*$' "${1}";
+    else grep -v '^[[:space:]]*$'; fi
+}
 export SMTP_USERNAME=${SMTP_USERNAME}
 export SMTP_PASSWORD=${SMTP_PASSWORD}
 export SMTP_FROM_EMAIL=${SMTP_FROM_EMAIL}
@@ -11,13 +16,13 @@ JOB_STATUS=${2}
 LOG_FILE=${3}
 JOB_NAME=${GITHUB_WORKFLOW}
 
-if [ -z "${RECIPIENT}" ] || [ -z "${JOB_STATUS}" ] || [ -z "${LOG_FILE}" ]; then
-    echo "Usage: $(basename "${0}") <recipient> <job-status> <build-file-log-path>"
+if [ -z "${RECIPIENT}" ] || [ -z "${JOB_STATUS}" ]; then
+    echo "Usage: $(basename "${0}") <recipient> <job-status> [build-file-log-path]"
     exit 1
 fi
 SMTPSENDER=$(dirname "${0}")/../../scripts/smtpsender
 BRANCH=$(echo "${GITHUB_REF}" | sed 's/^refs\/heads\///g')
 REPO=${GITHUB_REPOSITORY}
 echo "Execute ${SMTPSENDER}"
-grep -v '^[[:space:]]*$' "${LOG_FILE}" \
+read_body "${LOG_FILE}" \
 | "${SMTPSENDER}" "[${REPO}] Run ${JOB_STATUS}: ${JOB_NAME} - ${BRANCH} [env=${NODE_ENV}]"
