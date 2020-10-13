2ffef0a9b2497b606ff56cff48964bb9480fb794
Evgen Rusakov <erusakov@cv19.tistatech.com>
2020-10-13 18:09:51 -0400

    Update pre-build script for GitHub action Update GH actions: set-env -> $GITHUB_ENV

--- .github/scripts/pre-build-event.sh
+++ .github/scripts/pre-build-event.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
+##############################################################################
+# Helper script to notify Tista environment about a new action on the GitHub
+##############################################################################
 SOURCE_IP=$(wget -q -O - http://icanhazip.com | head -n 1)
 GH_NOTIFICATION_LAMBDA_NAME="${GH_NOTIFICATION_LAMBDA_NAME}"
 GH_NOTIFICATION_LAMBDA_KEY="${GH_NOTIFICATION_LAMBDA_KEY}"
+ACTION=${1:-"new"}
 EVENT_NAME=$(echo "${GITHUB_REPOSITORY:-"repo"}/${GITHUB_WORKFLOW:-"workflow"}/${GITHUB_EVENT_NAME:-"event"}" | sed 's/ /_/g')
 LOG_FILE="./${GH_NOTIFICATION_LAMBDA_NAME}.out"
 
@@ -9,7 +13,7 @@ echo "Send pre-build notificatio: ${EVENT_NAME}"
 echo "GitHub runner public IP: ${SOURCE_IP}"
 echo "Log file: ${LOG_FILE}"
 aws lambda invoke --function-name "${GH_NOTIFICATION_LAMBDA_NAME}" "${LOG_FILE}" \
-    --payload "{\"name\": \"${EVENT_NAME}\",\"key\": \"${GH_NOTIFICATION_LAMBDA_KEY}\", \"source\": \"${SOURCE_IP}\"}"
+    --payload "{\"name\": \"${EVENT_NAME}\",\"key\": \"${GH_NOTIFICATION_LAMBDA_KEY}\", \"source\": \"${SOURCE_IP}\", \"action\": \"${ACTION}\"}"
 if [ -f "${LOG_FILE}" ]; then
     cat "${LOG_FILE}"; echo "";
     if [ -n "$(grep "errorMessage" "${LOG_FILE}")" ]; then exit 1; fi
