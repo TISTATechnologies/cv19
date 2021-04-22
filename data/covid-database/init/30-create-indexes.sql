607d8e2c09b7b185e718eb7c474ad117b09e52d7
Evgen Rusakov <erusakov@cv19.tistatech.com>
2021-04-22 14:29:11 -0400

    Add additional indexes into the DB on the materialized views

--- data/covid-database/init/30-create-indexes.sql
+++ data/covid-database/init/30-create-indexes.sql
@@ -12,3 +12,15 @@ CREATE INDEX ON region (fips);
 CREATE INDEX ON region_population (country_id);
 CREATE INDEX ON region_population (state_id);
 CREATE INDEX ON region_population (fips);
+
+CREATE INDEX ON covid_data_stat (country_id);
+CREATE INDEX ON covid_data_stat (state_id);
+CREATE INDEX ON covid_data_stat (fips);
+CREATE INDEX ON covid_data_stat (date);
+CREATE INDEX ON covid_data_stat (location_type);
+
+CREATE INDEX ON covid_data_stat_latest (country_id);
+CREATE INDEX ON covid_data_stat_latest (state_id);
+CREATE INDEX ON covid_data_stat_latest (fips);
+CREATE INDEX ON covid_data_stat_latest (date);
+CREATE INDEX ON covid_data_stat_latest (location_type);
